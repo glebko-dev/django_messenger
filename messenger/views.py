570a45d2276af7795a370b30ce2bb8e3ac76ad88
Gleb <glehont@gmail.com>
2026-04-06 19:33:12 +0300

    Добавлена главная страница

--- messenger/views.py
+++ messenger/views.py
@@ -3,20 +3,20 @@ from django.shortcuts import render, redirect
 
 def index(request):
     if request.user.is_authenticated:
-        pass
+        return render(request, 'index.html')
 
     return redirect('sign_in')
 
 
 def sign_in(request):
     if request.method == 'POST':
-        pass
+        return redirect('index')
 
     return render(request, 'sign_in.html')
 
 
 def sign_up(request):
     if request.method == 'POST':
-        pass
+        return redirect('index')
 
     return render(request, 'sign_up.html')

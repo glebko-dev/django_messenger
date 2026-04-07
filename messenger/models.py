94e75c6e884ecfb854fd5ced7df564fcc9ccb936
Gleb <glehont@gmail.com>
2026-04-07 13:50:51 +0300

    Добавлены модели чата и сообщения

--- messenger/models.py
+++ messenger/models.py
@@ -1,6 +1,16 @@
-from django.db.models import EmailField
+from django.db.models import Model, ManyToManyField, ForeignKey, EmailField, CASCADE
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
     email = EmailField(blank=True, null=True)
+
+
+class Chat(Model):
+    users = ManyToManyField(User)
+
+
+class Message(Model):
+    sender = ForeignKey(User, on_delete=CASCADE)
+
+    chat = ForeignKey(Chat, on_delete=CASCADE)
